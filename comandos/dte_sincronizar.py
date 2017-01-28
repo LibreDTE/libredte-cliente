30cbc04072b3ab8b90f564c189becae4585a312f
Esteban De La Fuente Rubio <esteban@delaf.cl>
2017-01-28 03:50:45 -0300

    Comando para enviar XML generados localmente al servidor de LibreDTE

--- comandos/dte_sincronizar.py
+++ comandos/dte_sincronizar.py
@@ -27,131 +27,64 @@ Comando para sincronizar documentos emitidos de manera local con el servidor de 
 
 # módulos usados
 from base64 import b64encode, b64decode
-import os
 from json import dumps as json_encode
-from json import loads as json_decode
+import os
 import codecs
+
+# opciones en formato corto
+options = 'v'
 
 # opciones en formato largo
 long_options = ['dir=']
 
+# cliente de LibreDTE
+LibreDTE = None
+
+# verbose
+verbose = False
+
 # función principal del comando
 def main(Cliente, args, config) :
-    print('Sincronizando')
-"""
-    json, xml, archivo, formato, cedible, papel, web, dir, normalizar, getXML = parseArgs(args)
-    data = None
-    if json :
-        data = loadJSON(json)
-        formato = 'json'
-    if xml :
-        data = loadXML(xml)
-        formato = 'xml'
-    if archivo != None and formato != None and formato not in ('json', 'xml') :
-        data = '"'+b64encode(bytes(loadFile(archivo), 'UTF8')).decode('UTF8')+'"'
-    if data == None :
-        print('Debe especificar un archivo JSON o bien un archivo XML a enviar')
+    global LibreDTE
+    global verbose
+    dir, verbose = parseArgs(args)
+    LibreDTE = Cliente
+    if not dir or not os.path.isdir(dir) :
+        print('Debes indicar un directorio (--dir) que se sincronizará con el servidor de LibreDTE')
         return 1
-    if not dir :
-        print('Debe especificar un directorio de salida para los archivos a generar')
-        return 1
-    # crear directorio para archivos si no existe
-    if not os.path.exists(dir):
-        os.makedirs(dir)
-    # crear DTE temporal
-    emitir = Cliente.post('/dte/documentos/emitir?normalizar='+str(normalizar)+'&formato='+formato, data)
-    if emitir.status_code!=200 :
-        print('Error al emitir DTE temporal: '+json_encode(emitir.json()))
-        return emitir.status_code
-    try :
-        with open(dir+'/temporal.json', 'w') as f:
-            f.write(json_encode(emitir.json()))
-    except ValueError :
-        print('Error al recibir JSON DTE temporal, se recibió: '+emitir.text)
-        return 1
-    # crear DTE real
-    generar = Cliente.post('/dte/documentos/generar?getXML='+str(getXML), emitir.json())
-    if generar.status_code!=200 :
-        print('Error al generar DTE real: '+json_encode(generar.json()))
-        return generar.status_code
-    try :
-        dte_emitido = generar.json()
-    except ValueError :
-        print('Error al recibir JSON DTE real, se recibió: '+generar.text)
-        return 1
-    xml_emitido = dte_emitido['xml']
-    dte_emitido['xml'] = None
-    with open(dir+'/emitido.json', 'w') as f:
-        f.write(json_encode(dte_emitido))
-    if getXML :
-        with codecs.open(dir+'/emitido.xml', 'w', 'iso-8859-1') as f:
-            f.write(b64decode(xml_emitido).decode('iso-8859-1'))
-    columnas = ['emisor', 'dte', 'folio', 'certificacion', 'tasa', 'fecha', 'sucursal_sii', 'receptor', 'exento', 'neto', 'iva', 'total', 'usuario', 'track_id']
-    valores = []
-    for col in columnas :
-        if dte_emitido[col] != None :
-            valores.append(str(dte_emitido[col]))
-        else :
-            valores.append('')
-    with open(dir+'/emitido.csv', 'w') as f:
-        f.write(';'.join(columnas)+"\n")
-        f.write(';'.join(valores)+"\n")
-    # obtener el PDF del DTE
-    generar_pdf = Cliente.get('/dte/dte_emitidos/pdf/'+str(generar.json()['dte'])+'/'+str(generar.json()['folio'])+'/'+str(generar.json()['emisor'])+'?cedible='+str(cedible)+'&papelContinuo='+str(papel))
-    if generar_pdf.status_code!=200 :
-        print('Error al generar PDF del DTE: '+json_encode(generar_pdf.json()))
-        return generar_pdf.status_code
-    # guardar PDF en el disco
-    with open(dir+'/emitido.pdf', 'wb') as f:
-        f.write(generar_pdf.content)
+    archivos_xml = [f for f in os.listdir(dir) if os.path.isfile(os.path.join(dir, f)) and f[-4:].lower()=='.xml']
+    for xml in archivos_xml :
+        enviarXML(dir, xml)
     return 0
 
 # función que procesa los argumentos del comando
 def parseArgs(args) :
-    json = ''
-    xml = ''
-    archivo = None
-    formato = None
-    cedible = 1
-    papel = 0
-    web = False
-    dir = ''
-    normalizar = 1
-    getXML = 0
+    dir = None
+    verbose = 0
     for var, val in args:
-        if var == '--json' :
-            json = val
-        elif var == '--xml' :
-            xml = val
-        elif var == '--archivo' :
-            archivo = val
-        elif var == '--formato' :
-            formato = val
-        elif var == '--cedible' :
-            cedible = val
-        elif var == '--papel' :
-            papel = val
-        elif var == '--web' :
-            web = val
-        elif var == '--dir' :
+        if var == '--dir' :
             dir = val
-        elif var == '--normalizar' :
-            normalizar = val
-        elif var == '--getXML' :
-            getXML = 1
-    return json, xml, archivo, formato, cedible, papel, web, dir, normalizar, getXML
+        if var == '-v' :
+            verbose = len(val)
+    return dir, verbose
 
-# función que carga un JSON
-def loadJSON (archivo) :
-    return json_decode(loadFile(archivo))
+def enviarXML(dir_path, archivo_xml) :
+    global LibreDTE
+    global verbose
+    xml = loadXML(dir_path+'/'+archivo_xml)
+    xml_base64 = json_encode(b64encode(bytes(xml, 'iso-8859-1')).decode('iso-8859-1'))
+    cargar = LibreDTE.post('/dte/dte_emitidos/cargar_xml', xml_base64)
+    if cargar.status_code!=200 :
+        print('Error al cargar el XML '+archivo_xml+': '+json_encode(cargar.json()))
+        return cargar.status_code
+    elif verbose :
+        print('Archivo XML '+archivo_xml+' fue cargado con éxito')
 
 # función que carga un XML
-def loadXML (archivo) :
-    return '"'+b64encode(bytes(loadFile(archivo), 'UTF8')).decode('UTF8')+'"'
-
-# función que carga un archivo
-def loadFile (archivo) :
-    with open(archivo, 'r') as content_file:
-        content = content_file.read()
-    return content
-"""
+def loadXML (archivo_xml) :
+    datos_xml = ''
+    fd = codecs.open(archivo_xml, 'r', 'iso-8859-1')
+    for linea in fd :
+        datos_xml += linea
+    fd.close()
+    return datos_xml
