3cc9c343bf72600ab83a8d645908a73a8b675c8c
Esteban De La Fuente Rubio <esteban@delaf.cl>
2017-02-17 18:32:39 -0300

    Se agrega soporte para imprimir en GNU/Linux

--- comandos/imprimir.py
+++ comandos/imprimir.py
@@ -21,35 +21,36 @@ En caso contrario, consulte <http://www.gnu.org/licenses/agpl.html>.
 
 """
 Comando para imprimir en la impresora por defecto y en una impresora en específico
-@author TODO
-@version 2017-01-11
+@author Fernando Lizana Nuñez (f.lizananuez[at]uandresbello.edu)
+@author Esteban De La Fuente Rubio, DeLaF (esteban[at]sasco.cl)
+@version 2017-02-17
 """
-'''
-se utilizó pywin32-220.win32-py3.5.exe
-'''
+
 # módulos usados por el comando
-from lxml import objectify
 import os
-import win32print
-import win32api
-from time import sleep 
-# opciones en formato corto
-options = ''
+if os.name == 'posix':
+    import cups
+elif os.name == 'nt' :
+    import win32print
+    import win32api
+    from time import sleep
 
 # opciones en formato largo
 long_options = ['pdf=', 'impresora=']
 
 # función principal del comando
-def main (Cliente, args) :
+def main (Cliente, args, config) :
     pdf,impresora = parseArgs(args)
-    if impresora == None :
+    if impresora is None :
         impresora = getDefaultPrinter()
-    if os.name == 'nt' :
-        return printWindows(pdf,impresora);
-    else:
-        if os.name == 'posix':
-            return printLinux(pdf,impresora)
-    print('No soportado')
+    if impresora is None :
+        print('No hay impresora disponible')
+        return 2
+    if os.name == 'posix':
+        return printLinux(pdf, impresora)
+    elif os.name == 'nt' :
+        return printWindows(pdf, impresora);
+    print('Sistema', os.name, 'no soportado')
     return 1
 
 # función que procesa los argumentos del comando
@@ -62,15 +63,32 @@ def parseArgs(args) :
         if var == '--impresora' :
             impresora = val
     return pdf, impresora
+
+# entrega la impresora por defecto del sistema
 def getDefaultPrinter() :
-    Default=win32print.GetDefaultPrinter()#Función que entrega un string con el nombre de la impresora
-    return Default
-def printWindows(pdf,impresora) :
-    ImpresoraPorDefecto = str(win32print.GetDefaultPrinter())#Primero guardamos la impresora por defecto
-    win32print.SetDefaultPrinter(impresora)#Luego se cambia la impresora por defecto por la impresora específica
-    win32api.ShellExecute(0,"print",pdf,None,".",0)
-    sleep(5)#Se espera un tiempo para que se envíe el archivo a la impresora
-    win32print.SetDefaultPrinter(ImpresoraPorDefecto)#Vuelve a estar la impresora por defecto original
+    if os.name == 'nt' :
+        defaultPrinter = win32print.GetDefaultPrinter()#Función que entrega un string con el nombre de la impresora
+    else :
+        conn = cups.Connection()
+        defaultPrinter = conn.getDefault()
+        if defaultPrinter is None :
+            printers = conn.getPrinters()
+            for printer in printers :
+                defaultPrinter = printer
+                break
+    return defaultPrinter
+
+# imprimir en linux
 def printLinux(pdf, impresora) :
-    print('No soportado')
-    return 1
+    conn = cups.Connection()
+    conn.printFile(impresora, pdf, 'DTE', {})
+    return 0
+
+# imprimir en windows
+def printWindows(pdf, impresora) :
+    ImpresoraPorDefecto = str(win32print.GetDefaultPrinter()) # primero guardamos la impresora por defecto
+    win32print.SetDefaultPrinter(impresora) # luego se cambia la impresora por defecto por la impresora específica
+    win32api.ShellExecute(0, "print", pdf, None, ".", 0)
+    sleep(5) # se espera un tiempo para que se envíe el archivo a la impresora
+    win32print.SetDefaultPrinter(ImpresoraPorDefecto) # vuelve a estar la impresora por defecto original
+    return 0
